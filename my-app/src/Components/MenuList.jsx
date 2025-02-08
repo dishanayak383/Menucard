a69794851d0d953dcdc8ba162f9374a7374fa188
dishanayak383 <dishanayak383@gmail.com>
2025-02-08 10:21:31 +0530

    Initial commit

--- my-app/src/Components/MenuList.jsx
+++ my-app/src/Components/MenuList.jsx
@@ -1,4 +1,4 @@
- import React, { useState } from "react";
+import React, { useState } from "react";
 import "./menulist.css";
 
 const menuItems = [
@@ -6,52 +6,53 @@ const menuItems = [
     name: "Biscuit Mama with Susu",
     price: "Rp 60.000,00",
     image: "https://plus.unsplash.com/premium_photo-1670253258590-9bfea76faacd?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8YmlzY3VpdHN8ZW58MHx8MHx8fDA%3D",
-    available: 10,
+   
   },
   {
     name: "Kroasang Thats it",
     price: "Rp 78.000,00",
-    image: "https://vincentcafe.pl/wp-content/uploads/2022/11/croissant-z-czekoladajpg.jpg",
-    available: 8,
+    image: "https://vincentcafe.pl/wp-content/uploads/2022/11/croissant-z-czekoladajpg.jpg",  
+  
   },
   {
     name: "Strawberry Float",
     price: "Rp 45.000,00",
     image: "/images/strawberry.jpg",
-    available: 5,
+   
   },
   {
     name: "Healthy Kids Meal",
     price: "Rp 83.000,00",
     image: "/images/kids-meal.jpg",
-    available: 6,
+   
   },
   {
     name: "Mangoo Cereal Milk",
     price: "Rp 95.000,00",
     image: "/images/mangoo milk.jpg",
-    available: 6,
+  
   },
   {
     name: "Smoothie Brown Berry",
     price: "Rp 95.000,00",
     image: "/images/Smoothie Brown Berry",
-    available: 2,
+   
   },
   {
     name: "Rainbow Cupcake",
     price: "Rp 95.000,00",
-    image:"/images/Rainbow Cupcake",
-    available: 10,
+    image:"https://img.freepik.com/premium-photo/soft-pastel-cupcakes-displayed-vintage-wallpaper_987764-81576.jpg",
+   
   },
   {
     name: "Mie Gocengan Spicy",
     price: "Rp 95.000,00",
     image: "https://p16-va.lemon8cdn.com/tos-alisg-v-a3e477-sg/oIiX6AWztE1zBARAiwpqB2SCQOtfIy3AeDRgEB~tplv-tej9nj120t-origin.webp",
-    available: 6,
+   
   },
   
 ];
+
 
 const MenuCard = ({ item }) => {
   const [quantity, setQuantity] = useState(0);
@@ -67,7 +68,7 @@ const MenuCard = ({ item }) => {
         <span>{quantity}</span>
         <button onClick={() => setQuantity(quantity + 1)}>+</button>
       </div>
-      <button className="order-button">Order</button>
+
     </div>
   );
 };
@@ -83,4 +84,5 @@ const MenuList = () => {
 };
 
 export default MenuList;
+
 
